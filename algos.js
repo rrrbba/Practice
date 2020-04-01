7add94fc6f9a8c4f9e2035796ce65e4dc3ac0e3b
Rayven <rayven.burns@gmail.com>
2020-04-01 00:00:40 -0400

    attempted recursive bs, will have to come back to it

--- algos.js
+++ algos.js
@@ -19,7 +19,7 @@ const binary_search = (arr, target) => {
     //while haven't narrowed down to one element
     while (low <= high) {
         //initialize middle
-        let mid = Math.floor((low + high)/2)
+        let mid = Math.floor((low + high)/2) //or (low + high) // 2
         //set variable guess
         let guess = arr[mid]
 
@@ -43,3 +43,29 @@ const binary_search = (arr, target) => {
 
 my_list = [2,3,5,7,9]
 console.log(binary_search(my_list, 9))
+
+//Recursive version of binary search
+const bs_recursive = (arr, target, low, high) => {
+
+    let mid = Math.floor((low + high)/2)
+    let guess = arr[mid]
+
+    if (arr.length == 0) {
+        return -1
+    }
+    if (low > high) {
+        return -1
+    }
+
+    if (guess === target) {
+        return mid
+    }
+
+    if (guess > target) {
+        return bs_recursive(arr, target, low, mid-1)
+    } else {
+        return bs_recursive(arr, target, mid+1, high)
+    }
+
+} 
+console.log(bs_recursive(my_list, 9, 2,9))
