62f47ae95039966cdb0566329d12e984311ed503
Rayven <rayven.burns@gmail.com>
2021-03-02 15:43:44 -0500

    fixed errors

--- efficiency.py
+++ efficiency.py
@@ -5,16 +5,16 @@ m = the number of properties per "manatee" (i.e. the number of keys in a manatee
 
 def example1(manatees):
     for manatee in manatees:
-        print manatee['name']
+        print (manatee['name'])
 
 def example2(manatees):
-    print manatees[0]['name']
-    print manatees[0]['age']
+    print (manatees[0]['name'])
+    print (manatees[0]['age'])
 
 def example3(manatees):
     for manatee in manatees:
         for manatee_property in manatee:
-            print manatee_property, ": ", manatee[manatee_property]
+            print (manatee_property, ": ", manatee[manatee_property])
 
 def example4(manatees):
     oldest_manatee = "No manatees here!"
@@ -24,7 +24,7 @@ def example4(manatees):
                 oldest_manatee = manatee2['name']
             else:
                 oldest_manatee = manatee1['name']
-    print oldest_manatee
+    print( oldest_manatee)
 
 
 
@@ -38,4 +38,4 @@ Example 3
 There are two for loops, and nested for loops are a good sign that you need to multiply two runtimes. Here, for every manatee, we check every property. If we had 4 manatees, each with 5 properties, then we would need 5+5+5+5 steps. This logic simplifies to the number of manatees times the number of properties, or O(nm).
 
 Example 4
-Again we have nested for loops. This time we're iterating over the manatees list twice - every time we see a manatee, we compare it to every other manatee's age. We end up with O(nn), or O(n^2) (which is read as "n squared").
+Again we have nested for loops. This time we're iterating over the manatees list twice - every time we see a manatee, we compare it to every other manatee's age. We end up with O(nn), or O(n^2) (which is read as "n squared")."""
