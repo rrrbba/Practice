5ae6fb587098c8c4a9dc7bca1f88d07391b1f448
Rayven <rayven.burns@gmail.com>
2021-03-05 00:17:32 -0500

    moved c++ to folder and updated comments to reflect that the file is now in a folder

--- cplusplus/Add2Numbers.cpp
+++ cplusplus/Add2Numbers.cpp
@@ -32,8 +32,12 @@ int main() {
 //Then
 //./Add2Numbers
 
+//if the cpp file is in a folder the command becomes
+//g++ -o Add2Numbers.exe cplusplus/Add2Numbers.cpp
+
 //For mac use
 //g++ -o Add2Numbers Add2Numbers.cpp
 
+//g++ os the name of the GCC C++ compiler
 //-o means option where you specify the output filename
 //and Add2Numbers.cpp is the input source file
